9f62bbd3799ccb7aa26952b0b4b1c354daf09173
SantiagoDomiro <dopretocoracao@gmail.com>
2020-12-30 20:19:38 -0600

    added probabilistic draft

--- booleanModel.R
+++ booleanModel.R
@@ -11,12 +11,22 @@ zebOR<-loadNetwork("Downloads/ZEB1.txt")#OR regulators
 attrAND <- getAttractors(zebAND)
 print(attrAND, activeOnly=TRUE)
 #recover all states in cancer basins
-cancer=lapply(c(4,6,8),function(x) 
+basins=lapply(1:8,function(x) 
 	as.data.frame(getBasinOfAttraction(attrAND,x)))
-temp=lapply(cancer,function(x) t(as.matrix(x[,1:9])))#columns with states
-heatmap(temp[[2]],Rowv=NA,scale="n",col=c("white","black"),
-	Colv=NA,labCol=NA,add.expr = abline(v=which(cancer[[2]][,20]==0),
-		col="red",lwd=2))
+#plot them
+temp=lapply(basins,function(x) t(as.matrix(x[,1:9])))#columns with states
+pdf("zeb1Basins.pdf")
+lapply(1:8,function(x) heatmap(temp[[x]],Rowv=NA,scale="n",
+	col=c("white","black"),Colv=NA,labCol=NA,
+	add.expr = abline(v=which(cancer[[x]][,20]==0),
+	col="red",lwd=2)))
+dev.off()
+#check hypoxia removal
+getPathToAttractor(zebAND,rep(0,9))
+#  Dicer miR200 miR205 ZEB1 EMT radioresistance hypoxia IR MeS
+#     0      0      0    0   0               0       0  0   0
+#     1      0      0    0   0               0       0  0   0
+#     1      1      1    0   0               0       0  0   0
 
 #get all attractors
 attrOR <- getAttractors(zebOR)
@@ -24,7 +34,7 @@ print(attrOR, activeOnly=TRUE)
 
 #getTransitionTable(attr) state transitions
 #getBasinOfAttraction(attr, 2) #states in a basin
-png("Downloads/ZEB1basin.png")
+png("Downloads/ZEB1attr.png")
 plotStateGraph(attrAND,layout=layout.svd,colorsAlpha=c(.3,.9,1,1))
 dev.off()
 
@@ -37,13 +47,40 @@ attrAND.1 <- getAttractors(zebAND,
  avoidSelfLoops=FALSE)
 print(attrAND.1, activeOnly=TRUE)
 #same as in synchronous
-#####################################check DE
-i=rbind(i,myannot[myannot$hgnc_symbol=="CDH1",])
-DE.i=lapply(DE.genes,function(x) x[rownames(x)%in%i$ensembl_gene_id,])
-DE.i=lapply(DE.i,function(x) x[order(match(rownames(x),i$ensembl_gene_id)),])
-DE.i=do.call(cbind,lapply(DE.i,function(x) x$logFC))
-rownames(DE.i)=i$hgnc_symbol
-colnames(DE.i)=gsub("_Normal","",colnames(DE.i))
+#####################################probabilistic model
+zebProb=loadNetwork("Downloads/ZEB1.txt")#Each functions has a probability, probs per var sum up to 1
+#get attractors
+sim <- markovSimulation(zebProb)
+g=plotPBNTransitions(sim,plotIt=F)#igraph
+#get basins
+basinsProb=components(g)$membership
+table(basinsProb)
+# 1  2  3  4  5  6  7  8 
+#64 64 64 64 64 64 64 64
+#same number of attractors & size of basins
+#png("Downloads/ZEB1attrProbabilistic.png")
+plot(g,vertex.label=NA,edge.label=NA,
+	vertex.color=basinsProb)
+dev.off()
+#format to plot
+basinsProb=sapply(1:8,function(x) names(which(basinsProb==x)))
+basinsProb=lapply(1:8,function(x) 
+	sapply(strsplit(basinsProb[,x],'*'),as.numeric))
+#compare to non-probabilistic basins
+sapply(1:8,function(x) sum(apply(basinsProb[[x]],2,paste,collapse="")
+	%in%apply(temp[[x]],2,paste,collapse="")))
+#[1] 64 64 64 64 64 64 64 64
+#pdf("Downloads/zeb1BasinsProba.pdf")
+#lapply(1:8,function(x) heatmap(basins[[x]],Rowv=NA,scale="n",
+#	col=c("white","black"),Colv=NA,labCol=NA))
+#dev.off()
+#but attractores are not the same????
+lapply(1:8,function(x) 
+	markovSimulation(zebProb,
+		startStates=basins[[x]][
+		which(basins[[x]]$transitionsToAttractor==0),
+		1:9])$reachedStates)
+
 
 #possible follwing steps
 #1 strong vs weak basins of atraction (for assync)
